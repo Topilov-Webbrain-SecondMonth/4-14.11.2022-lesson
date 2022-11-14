d2ae996adfe926252a240707198f29929073240d
topilovasilbek <asilbektback@gmail.com>
2022-11-14 22:48:31 +0500

    home practise for previous lesson ended

---
+++ main.js
@@ -0,0 +1,34 @@
+// console.log(true || 'asdas')
+// console.log(true && 'asdas')
+// console.log(false || 'asdas')
+// console.log(false && 'asdas')
+
+// const value1 = undefined ?? null;
+// console.log(value1);
+
+// if
+// let num = 4
+// if (num < 0) {
+//     console.log('manfiy son');
+// } else if (num === 0) {
+//     console.log('nolga teng');
+// } else {
+//     console.log('musbat son');
+// }
+
+
+// if (15 > 5)
+//     console.log("5 4dan katta");
+// if condition dan keyin then da bitta amal bo`ladigan bo`lsa, masalan, faqat
+// bitta console log bo`ladigan bo`lsa curly braces yozmasa bo`ladi
+
+// Ternary operator
+// let num = 100;
+// console.log(num > 110 ? 'katta' : num === 110 ? 'teng' : 'kichik');
+
+// Nullish coalescing ??
+// ?? returns the first argument if it’s not null/undefined. Otherwise, the second one.
+// console.log(123 ?? null); // 123
+// console.log('asdasd' ? '1'); // 'asdasd
+// console.log(null ?? null ?? undefined ?? null ?? undefined ?? 'Asilbek Topilov' ?? null);
+// We can use it in authentication
